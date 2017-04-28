3803a8688056eb22183e89bb11a59f31c926749c
Nandun Bandara <ntbandara3@hotmail.com>
2017-04-28 10:56:53 +0530

    userController valdiation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,10 +13,22 @@ exports.addUser = function(req,res){
     user.email = req.body.email;
     user.type = req.body.type;
     user.save(function(err){
-        if(err){
-            res.send('can not create user'+err);
+        if(req.body.username==null || req.body.username==""){
+            res.send("username not set");
+        }else if (req.body.password==null || req.body.password==""){
+            res.send("password not set");
+        }else if (req.body.name==null || req.body.name==""){
+            res.send("name not set");
+        }else if (req.body.email==null || req.body.email==""){
+            res.send("email not set");
+        }else if(req.body.type==null || req.body.type==""){
+            res.send("type not set");
         }else{
-            res.send('user created');
+            if(err){
+                res.send('can not create user'+err);
+            }else{
+                res.send('user created');
+            }
         }
     });
 }
