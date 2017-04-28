fc0261e5f257b6187ddb43cf5d6c2596832e7d94
Nandun Bandara <ntbandara3@hotmail.com>
2017-04-28 09:16:00 +0530

    user: controller and model

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,10 @@
 /**
  * Created by ntban_000 on 4/27/2017.
  */
+'use strict'
+
+const user = require('./models/user');
+
+exports.addUser = function(req,res){
+    
+}
--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,17 @@
 /**
  * Created by ntban_000 on 4/27/2017.
  */
+'use strict'
+
+const mongoose = require('mongoose');
+const  Scheme = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type:String, lowercase:true, required: true, unique:true },
+    password: { type: String, required:true },
+    name: { type:String, required:true },
+    email: { type:String, lowercase: true, required: true, unique:true },
+    type: { type:String, required:true }
+})
+
+module.exports = mongoose.model('User', UserSchema);
