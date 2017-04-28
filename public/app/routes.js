9d9dca82126003717b83fd2368974555ac404d8b
Nandun Bandara <ntbandara3@hotmail.com>
2017-04-28 19:50:16 +0530

    dashboard layout set

--- public/app/routes.js
+++ public/app/routes.js
@@ -3,13 +3,26 @@
  */
 angular.module('cyborgPharmacy.routes',['ui.router'])
 .config(function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise('/error');
+    // $urlRouterProvider.otherwise('/error');
     $stateProvider
 
         .state('login',{
             url: '/login',
             templateUrl: 'app/views/pages/users/login.html',
             controller: 'loginCtrl as login'
+        })
+
+        .state('dashboard',{
+            url:'/',
+            templateUrl: 'app/views/pages/dashboard.html'
+        })
+
+        .state('dashboard.addUser',{
+            url:'/addUser',
+            templateUrl: 'app/views/pages/users/addUser.html',
+            controller: function($scope){
+                $scope.message = "add user test";
+            }
         })
 
         .state('404', {
