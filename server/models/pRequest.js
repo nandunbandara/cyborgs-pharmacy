6d96c16e2b2ef1cb6e3fa3b081df89b7600ec8fd
ishan <ishanyapa@gmail.com>
2017-04-30 22:38:35 +0530

    Did some modifications to batch, drug and pRequest models. Now they are more organized than before.

--- server/models/pRequest.js
+++ server/models/pRequest.js
@@ -1,17 +1,17 @@
 /**
  * Created by ishan on 4/29/17.
  */
-'use strict'
+'use strict';
 
 const mongoose = require('mongoose');
 
 const requestSchema = new mongoose.Schema({
-    'rId':Number,
-    'name':String,
-    'drugCategory':String,
-    'drugPrice':Number,
-    'dQtyAvailable':Number,
-    'rAdded':String
+    'rId':{type:Number, required:true, unique:true},
+    'name':{type:String, required:true},
+    'drugCategory':{type:String, required:true},
+    'drugPrice':{type:Number, required:true},
+    'dQtyAvailable':{type:Number, required:true},
+    'rAdded':{type:String, required:true}
 });
 
 const PRequest = mongoose.model('PRequest',requestSchema);
