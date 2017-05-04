0da9d82f4535c5bc9b6828a416210559e20fbbeb
Nandun Bandara <ntbandara3@hotmail.com>
2017-05-04 11:59:54 +0530

    new user view

--- public/app/app.js
+++ public/app/app.js
@@ -21,17 +21,29 @@ angular.module('cyborgPharmacy', [
 
 
 .run( ['$rootScope','$location','Auth', function($rootScope, $location, Auth) {
-    $rootScope.$watch(function() {
-            return $location.path();
-        },
-        function(a){
-            if(!Auth.isLoggedIn()){
-                $location.path('/login');
-            }else{
-                //get user data
-                Auth.getUser().then(function(data){
-                    $rootScope.user = data;
-                })
-            }
-        });
+    // $rootScope.$watch(function() {
+    //         return $location.path();
+    //     },
+    //     function(a){
+    //         if(!Auth.isLoggedIn()){
+    //             $location.path('/login');
+    //         }else{
+    //             //get user data
+    //             Auth.getUser().then(function(data){
+    //                 $rootScope.user = data;
+    //             })
+    //         }
+    // });
+
+    $rootScope.$on('$routeChangeStart', function(event, next, current){
+        console.log("route changed")
+        if(!Auth.isLoggedIn()){
+            $location.path('/login');
+        }else{
+            //get user data
+            Auth.getUser().then(function(data){
+                $rootScope.user = data;
+            })
+        }
+    })
 }]);
