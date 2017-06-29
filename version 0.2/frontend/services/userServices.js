1d092ff1d10813f848fbeac2b154fcbedffb0ef9
root <ntbandara3@hotmail.com>
2017-06-29 20:37:14 +0530

    user services updated

--- version 0.2/frontend/services/userServices.js
+++ version 0.2/frontend/services/userServices.js
@@ -11,12 +11,34 @@ angular.module('userServices',[])
     userFactory.getUsers = function(){
         return $http.get(Conf.auth_service.concat('/users')).then(function(data){
             return data;
+        }).catch(function(err){
+            return err;
+        })
+    }
+
+    //get user by username
+    userFactory.getUserByUsername = function(uname){
+        return $http.get(Conf.auth_service.contact('/users/').contact(uname)).then(function(data){
+            return data;
+        }).catch(function(err){
+            return err;
         })
     }
 
     //create new user
     userFactory.addUser = function(data) {
         return $http.post(Conf.auth_service.concat('/users'),data).then(function(data){
+            return data;
+        }).catch(function(err){
+            return err;
+        })
+    }
+
+    //update user
+    userFactory.updateUser = function(uname,data){
+        return $http.put(Conf.auth_service.concat('/users/').concat(uname),data).then(function(data){
+            return data;
+        }).catch(function(err){
             return data;
         })
     }
@@ -25,6 +47,8 @@ angular.module('userServices',[])
     userFactory.deleteUser = function(username){
         return $http.delete(Conf.auth_service.concat('/users/').concat(username)).then(function(data){
             return data;
+        }).catch(function(err){
+            return err;
         })
     }
 
