a7e6278b7d91cce8ae95b60ba3cb0ce731224b3c
ishan <ishanyapa@gmail.com>
2017-07-01 14:36:50 +0530

    Mail sender implemented

--- version 0.2/frontend/services/confService.js
+++ version 0.2/frontend/services/confService.js
@@ -7,10 +7,10 @@ angular.module('cyborgPharmacy.confService',[])
     const con = {};
 
     con.auth_service = "http://localhost:9001";
-    con.drug_service = "http://35.164.154.55";
+    con.drug_service = "http://localhost:9002";
     con.prescription_service = "http://35.165.38.138";
     con.report_service = "http://34.212.249.156";
     //set your service URIs here
-
+    //http://35.164.154.55
     return con;
 }])
--- version 0.2/frontend/services/mailServices.js
+++ version 0.2/frontend/services/mailServices.js
@@ -4,3 +4,15 @@
 
 angular.module('mailServices',[])
 
+    .factory('EMail',['$http','Conf',function ($http,Conf) {
+        const mailFactory = [];
+
+        mailFactory.sendNewMail = function (data) {
+            return $http.post(Conf.drug_service.concat('/drug/mail'),data).then(function (res) {
+                return res;
+            })
+        }
+        return mailFactory;
+    }])
+
+
