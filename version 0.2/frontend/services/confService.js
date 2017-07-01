a3a71b179181a045915e6b526347283b61bddc28
root <ntbandara3@hotmail.com>
2017-07-01 14:31:36 +0530

    aws conf

--- version 0.2/frontend/services/confService.js
+++ version 0.2/frontend/services/confService.js
@@ -6,7 +6,7 @@ angular.module('cyborgPharmacy.confService',[])
 .factory('Conf', [function(){
     const con = {};
 
-    con.auth_service = "http://localhost:9001";
+    con.auth_service = "http://34.212.63.160";
     con.drug_service = "http://35.164.154.55";
     con.prescription_service = "http://35.165.38.138";
     con.report_service = "http://34.212.249.156";
