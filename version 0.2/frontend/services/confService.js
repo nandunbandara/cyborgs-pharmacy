01d3384560298abf7394498701fceb8e0ec0200d
nandunb <ntbandara3@hotmail.com>
2017-07-27 19:01:00 +0530

    service configuration update

--- version 0.2/frontend/services/confService.js
+++ version 0.2/frontend/services/confService.js
@@ -6,10 +6,10 @@ angular.module('cyborgPharmacy.confService',[])
 .factory('Conf', [function(){
     const con = {};
 
-    con.auth_service = "http://34.212.63.160";
-    con.drug_service = "http://35.164.154.55:6002";
-    con.prescription_service = "http://35.165.38.138";
-    con.report_service = "http://34.212.249.156";
+    con.auth_service = "http://127.0.0.1:9001";
+    con.drug_service = "http://127.0.0.1:9002";
+    con.prescription_service = "http://127.0.0.1:9003";
+    con.report_service = "http://127.0.0.1:9004";
     //set your service URIs here
     //http://35.164.154.55
     return con;
