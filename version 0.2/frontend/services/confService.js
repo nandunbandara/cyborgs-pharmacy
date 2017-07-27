1548bf11cf93accc9c3e9bcc103ae5301398ad9f
nandunb <ntbandara3@hotmail.com>
2017-07-27 16:28:24 +0530

    test commit new

--- version 0.2/frontend/services/confService.js
+++ version 0.2/frontend/services/confService.js
@@ -7,7 +7,7 @@ angular.module('cyborgPharmacy.confService',[])
     const con = {};
 
     con.auth_service = "http://34.212.63.160";
-    con.drug_service = "http://35.164.154.55";
+    con.drug_service = "http://35.164.154.55:6002";
     con.prescription_service = "http://35.165.38.138";
     con.report_service = "http://34.212.249.156";
     //set your service URIs here
